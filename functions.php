9a36bcebbe1ee5916b5f5d667168168ca0cf0614
Didier Grand <didier.grand@gmail.com>
2025-04-14 21:26:32 +0200

    disable_amelia_updates

--- functions.php
+++ functions.php
@@ -185,3 +185,12 @@ add_filter( 'wpcf7_form_tag', function( $tag ) {
 
     return $tag;
 });
+
+// Désactiver les mises à jour d'Amelia Booking
+add_filter('site_transient_update_plugins', 'disable_amelia_updates');
+function disable_amelia_updates($value) {
+    if (isset($value->response['ameliabooking/ameliabooking.php'])) {
+        unset($value->response['ameliabooking/ameliabooking.php']);
+    }
+    return $value;
+} 
